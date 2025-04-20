e69fd79c515aab81653ef9e1e5961cad5dc1cd3e
hkwln <p.aul@mail.de>
2025-04-20 20:16:57 +0200

    updated simplelist

--- Blatt_0/SimpleList.java
+++ Blatt_0/SimpleList.java
@@ -92,9 +92,10 @@ public class SimpleList<T> implements ISimpleListIterable<T> {
                 throw new NoSuchElementException();
             }
             T data = current.data;
-            for (int i = 0; i < step && current != null; i++) {
+            for (int i = 1; i < step && current != null; i++) { 
                 current = current.next;
             }
+            current = current.next; 
             return data;
         }
 
