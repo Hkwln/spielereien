95c4203f95ee663da1e9b3b65a2c8e00dc0efce5
Paulxocmd <sibila.paul2@gmail.com>
2025-04-30 07:59:13 +0200

    some minor issues

--- Mathe_spielereien/Spaß mit Zahlen/Millerrabin.c
+++ Mathe_spielereien/Spaß mit Zahlen/Millerrabin.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+/*
+Millerrabin.c:23:28: error: passing argument 1 of ‘realloc’ makes pointer from integer without a cast [-Wint-conversion]
+23 |             *arr = realloc(*arr, size * sizeof(int));
+   |                            ^~~~
+   |                            |
+   |                            int
+In file included from Millerrabin.c:2:
+/usr/include/stdlib.h:683:29: note: expected ‘void *’ but argument is of type ‘int’
+683 | extern void *realloc (void *__ptr, size_t __size)
+   |                       ~~~~~~^~~~~
+Millerrabin.c:23:18: error: assignment to ‘int’ from ‘void *’ makes integer from pointer without a cast [-Wint-conversion]
+23 |             *arr = realloc(*arr, size * sizeof(int));
+   |                  ^
+Millerrabin.c:24:19: error: subscripted value is neither array nor pointer nor vector
+24 |             (*arr)[size - 1] = temp;
+*/
 int generate_random(int min, int max){
     return min + rand() %(max - min +1);
 }
@@ -20,7 +35,7 @@ bool Millerrabin(int number){
         int size = 0;
         while (temp % 2 == 0) {
             size++;
-            arr = realloc(arr, size * sizeof(int));
+            *arr = realloc(*arr, size * sizeof(int));
             if (arr == NULL) {
                 fprintf(stderr, "Memory allocation failed\n");
                 exit(EXIT_FAILURE);
